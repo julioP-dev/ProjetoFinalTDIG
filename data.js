c40f4c02e24ec64403a89166073de44d687234a7
Júlio Pedro <juliop.dev@gmail.com>
2026-04-10 00:40:55 -0300

    add updates

---
+++ data.js
@@ -0,0 +1,63 @@
+const data = {
+  woofs: [
+    {
+      id: '1',
+      name: 'REX',
+      avatar: 'https://images.unsplash.com/photo-1587300003388-59208cc962cb?w=200&q=80',
+    },
+    {
+      id: '2',
+      name: 'BALL-R',
+      avatar: 'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?w=200&q=80',
+    },
+    {
+      id: '3',
+      name: 'HAPPY',
+      avatar: 'https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?w=200&q=80',
+    },
+    {
+      id: '4',
+      name: 'FLUFFY',
+      avatar: 'https://images.unsplash.com/photo-1552053831-71594a27632d?w=200&q=80',
+    },
+    {
+      id: '5',
+      name: 'BUDDY',
+      avatar: 'https://images.unsplash.com/photo-1477884213360-7e9d7dcc1e48?w=200&q=80',
+    },
+  ],
+  posts: [
+    {
+      id: '1',
+      title: 'HAPPY WOOFS',
+      description: 'How to keep your Woof happy and healthy. We\'ve asked some of the best vets.',
+      image: 'https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?w=300&q=80',
+    },
+    {
+      id: '2',
+      title: 'WOOFS & FRIENDS',
+      description: 'Best friends are important for humans, but also for dogs.',
+      image: 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=300&q=80',
+    },
+    {
+      id: '3',
+      title: 'GOOD WOOFS',
+      description: 'When they behave right, they are not a danger to society.',
+      image: 'https://images.unsplash.com/photo-1589941013453-ec89f33b5e95?w=300&q=80',
+    },
+    {
+      id: '4',
+      title: 'WOOF NUTRITION',
+      description: 'The best diet for your furry friend depends on their breed and age.',
+      image: 'https://images.unsplash.com/photo-1477884213360-7e9d7dcc1e48?w=300&q=80',
+    },
+    {
+      id: '5',
+      title: 'OUTDOOR WOOFS',
+      description: 'Tips for the best adventures with your dog in nature.',
+      image: 'https://images.unsplash.com/photo-1552053831-71594a27632d?w=300&q=80',
+    },
+  ],
+};
+
+export default data;
